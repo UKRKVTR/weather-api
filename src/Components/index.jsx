85f374de08ea72466343b188ec0288b65e87c714
Danylo <warfais6@gmail.com>
2023-11-01 20:02:34 +0200

    Optimized Work

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -6,8 +6,9 @@ function Weather() {
   const [weatherData, setWeatherData] = useState({});
   const [celsius, setCelsius] = useState("celsius");
   const [units, setUnits] = useState("km");
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current=temperature_2m,windspeed_10m&hourly=temperature_2m${
         celsius === "celsius" ? "" : "&temperature_unit=fahrenheit"
@@ -17,7 +18,10 @@ function Weather() {
       .then((data) => {
         setWeatherData(data);
       })
-      .catch((err) => console.log("Error fetching data:", err));
+      .catch((err) => console.log("Error:", err))
+      .finally(() => {
+        setLoading(false);
+      });
   }, [celsius, units]);
 
   const temperatureC = weatherData?.current?.temperature_2m;
@@ -31,26 +35,12 @@ function Weather() {
   function handleSpeedChange(value) {
     setUnits(value);
   }
-  return (
-    <article>
-      <div className={styles.main}>
-        <div className={styles.select}>
-          <select
-            defaultValue={celsius}
-            onChange={(e) => handleCelsiusChange(e.target.value)}
-          >
-            <option value="celsius">°C</option>
-            <option value="fahrenheit">°F</option>
-          </select>
-          <select
-            defaultValue={celsius}
-            onChange={(e) => handleSpeedChange(e.target.value)}
-          >
-            <option value="km">km/h</option>
-            <option value="m">m/s</option>
-          </select>
-        </div>
-        <div>
+  function display() {
+    if (loading) {
+      return <div className={styles.loading}>Загрузка...</div>;
+    } else {
+      return (
+        <>
           <div>
             {temperatureC}
             {unitsC}
@@ -59,7 +49,31 @@ function Weather() {
             {speedKmH}
             {unitsKmH}
           </div>
+        </>
+      );
+    }
+  }
+  return (
+    <article>
+      <div className={styles.main}>
+        <div className={styles.select}>
+          {" "}
+          <select
+            defaultValue={celsius}
+            onChange={(e) => handleCelsiusChange(e.target.value)}
+          >
+            <option value="celsius">°C</option>
+            <option value="fahrenheit">°F</option>
+          </select>
+          <select
+            defaultValue={units}
+            onChange={(e) => handleSpeedChange(e.target.value)}
+          >
+            <option value="km">km/h</option>
+            <option value="m">m/s</option>
+          </select>{" "}
         </div>
+        <div>{display()}</div>
       </div>
     </article>
   );
